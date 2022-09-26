3a26180cacf14cc9b27163898cd64760d2f0864c
Barysh-AR <91249485+Barysh-AR@users.noreply.github.com>
2022-09-26 18:50:37 +0300

    Update Main.java

--- StringCalculator/src/Main.java
+++ StringCalculator/src/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int i = 0;
 
         Scanner scan = new Scanner(System.in);
         String  str = scan.nextLine();
